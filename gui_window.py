211be2bbf02988ba74fb8bcdc28a126b4e2db07b
yger <pierre.yger@gmail.com>
2020-06-22 20:51:50 +0200

    WIP

--- gui_window.py
+++ gui_window.py
@@ -32,7 +32,7 @@ from circusort.obj.train import Train
 from circusort.obj.amplitude import Amplitude
 
 
-_all_views_ = [TraceCanvas]
+_all_views_ = [TraceCanvas, MEACanvas, RateCanvas, ISICanvas, TemplateCanvas]
 
 class InfoController(Controler):
 
